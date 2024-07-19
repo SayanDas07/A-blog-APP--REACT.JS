696501537ea7c78a0cb4c10cbc8be196dbf5c299
Sayan Das <sayandasraja56@gmail.com>
2024-07-19 20:00:19 +0530

    Add RTE and PostForm

--- MegaProject1/src/components/index.js
+++ MegaProject1/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
 
-export { Header, Footer, Logo, Container, LogoutBtn }; 
+export { Header, Footer, Logo, Container, LogoutBtn, RTE }; 

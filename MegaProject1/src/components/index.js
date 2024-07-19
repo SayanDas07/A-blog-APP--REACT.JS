1a9945b244dba904c589d4af17cd40f223d12e4b
Sayan Das <sayandasraja56@gmail.com>
2024-07-20 02:21:48 +0530

    Add Pages

--- MegaProject1/src/components/index.js
+++ MegaProject1/src/components/index.js
@@ -4,5 +4,9 @@ import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./header/LogoutBtn.jsx";
 import RTE from "./RTE.jsx";
+import SignUp from "./SignUp.jsx";
+import Login  from "./Login.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx";
 
-export { Header, Footer, Logo, Container, LogoutBtn, RTE }; 
+export { Header, Footer, Logo, Container, LogoutBtn, RTE, SignUp, Login, PostForm, PostCard }; 
